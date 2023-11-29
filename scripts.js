408dd3648e2699ca28af77c0102498dd5c88622f
lucreciazpiazu <144922319+lucreciazpiazu@users.noreply.github.com>
2023-11-29 15:38:10 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,18 @@
-// MODELO DE DATOS
+import { createClient } from '@supabase/supabase-js';
 
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://lfepjfbynjdipsesbtdc.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmZXBqZmJ5bmpkaXBzZXNidGRjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMTI1ODMwMCwiZXhwIjoyMDE2ODM0MzAwfQ.ZwFKdV1kIUtUL2QAp4aJrhBaxw7XxpgcAyWf-0JicHc'
-const supabase = createClient(supabaseUrl, supabaseKey)
-
+// Configura Supabase con tus credenciales
+const supabaseUrl = 'https://lfepjfbynjdipsesbtdc.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmZXBqZmJ5bmpkaXBzZXNidGRjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMTI1ODMwMCwiZXhwIjoyMDE2ODM0MzAwfQ.ZwFKdV1kIUtUL2QAp4aJrhBaxw7XxpgcAyWf-0JicHc';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // MODELO DE DATOS
 let mis_peliculas_iniciales = [
- let mis_peliculas_iniciales = [
     {titulo: "Interstellar",  director: "Christopher Nolan", "miniatura": "files/interstellar.png"},
     {titulo: "Jurassic Park", director: "Steven Spielberg", "miniatura": "files/jurassicpark.png"},
     {titulo: "Superlópez",   director: "Javier Ruiz Caldera", "miniatura": "files/superlopez.png"}
 ];
 
-// ...
+// Verificar si ya hay datos en Supabase, si no, insertar los datos iniciales
 async function setupSupabase() {
     try {
         const { data, error } = await supabase.from('peliculas').select('*');
@@ -28,61 +26,103 @@ async function setupSupabase() {
             await supabase.from('peliculas').insert(mis_peliculas_iniciales);
         }
     } catch (error) {
-        console.error('Error durante la configuración de Supabase:', error);
+        console.error('Error durante la configuración de Supabase:', error.message);
     }
 }
-// ...
-
-localStorage.mis_peliculas = localStorage.mis_peliculas || JSON.stringify(mis_peliculas_iniciales);
 
 // VISTAS
 const indexView = (peliculas) => {
     let i = 0;
-    let i=0;
     let view = "";
 
     while (i < peliculas.length) {
         view += `
-    while(i < peliculas.length) {
-      view += `
         <div class="movie">
            <div class="movie-img">
                 <img data-my-id="${i}" src="${peliculas[i].miniatura}" onerror="this.src='files/placeholder.png'"/>
-@@ -52,8 +28,8 @@ const indexView = (peliculas) => {
+           </div>
+           <div class="title">
+               ${peliculas[i].titulo || "<em>Sin título</em>"}
+           </div>
+           <div class="actions">
+               <button class="edit" data-my-id="${i}">editar</button>
+               <button class="delete" data-my-id="${i}">borrar</button>
                <button class="show" data-my-id="${i}">ver</button>
             </div>
         </div>\n`;
         i = i + 1;
     }
-      i = i + 1;
-    };
 
     view += `<div class="actions">
                 <button class="new">añadir</button>
-@@ -88,7 +64,7 @@ const editView = (i, pelicula) => {
+                <button class="reset">reset</button>
+            </div>`;
+
+    return view;
+};
+
+const editView = (i, pelicula) => {
+    return `<h2>Editar Película </h2>
+        <div class="field">
+        Título <br>
+        <input  type="text" id="titulo" placeholder="Título" 
+                value="${pelicula.titulo}">
+        </div>
+        <div class="field">
+        Director <br>
+        <input  type="text" id="director" placeholder="Director" 
+                value="${pelicula.director}">
+        </div>
+        <div class="field">
+        Miniatura <br>
+        <input  type="text" id="miniatura" placeholder="URL de la miniatura" 
+                value="${pelicula.miniatura}">
+        </div>
+        <div class="actions">
+            <button class="update" data-my-id="${i}">
+                Actualizar
+            </button>
+            <button class="index">
                 Volver
             </button>
        `;
 };
-}
 
 const showView = (pelicula) => {
     return `
-@@ -100,7 +76,7 @@ const showView = (pelicula) => {
+     <h2>${pelicula.titulo}</h2>
+     <p>Director: ${pelicula.director}</p>
+     <div class="movie-img">
+        <img src="${pelicula.miniatura}" onerror="this.src='files/placeholder.png'"/>
+     </div>
      <div class="actions">
         <button class="index">Volver</button>
      </div>`;
 };
-}
 
 const newView = () => {
     return `<h2>Crear Película</h2>
-@@ -124,138 +100,64 @@ const newView = () => {
+        <div class="field">
+        Título <br>
+        <input  type="text" id="titulo" placeholder="Título">
+        </div>
+        <div class="field">
+        Director <br>
+        <input  type="text" id="director" placeholder="Director">
+        </div>
+        <div class="field">
+        Miniatura <br>
+        <input  type="text" id="miniatura" placeholder="URL de la miniatura">
+        </div>
+        <div class="actions">
+            <button class="create">
+                Crear
+            </button>
+            <button class="index">
                 Volver
             </button>
         </div>`;
 };
-}
 
 // CONTROLADORES 
 const indexContr = async () => {
@@ -90,7 +130,7 @@ const indexContr = async () => {
         await setupSupabase();
 
         const { data: peliculas, error } = await supabase.from('peliculas').select('*');
-
+        
         if (error) {
             throw error;
         }
@@ -99,15 +139,12 @@ const indexContr = async () => {
     } catch (error) {
         console.error('Error al obtener las películas:', error.message);
     }
-const indexContr = () => {
-    let mis_peliculas = JSON.parse(localStorage.mis_peliculas);
-    document.getElementById('main').innerHTML = indexView(mis_peliculas);
 };
 
 const showContr = async (i) => {
     try {
         const { data: peliculas, error } = await supabase.from('peliculas').select('*');
-
+        
         if (error) {
             throw error;
         }
@@ -116,9 +153,6 @@ const showContr = async (i) => {
     } catch (error) {
         console.error('Error al obtener la película:', error.message);
     }
-const showContr = (i) => {
-    let mis_peliculas = JSON.parse(localStorage.mis_peliculas);
-    document.getElementById('main').innerHTML = showView(mis_peliculas[i]);
 };
 
 const newContr = () => {
@@ -143,22 +177,12 @@ const createContr = async () => {
     } catch (error) {
         console.error('Error al crear la película:', error.message);
     }
-const createContr = () => {
-    let nuevaPelicula = {
-        titulo: document.getElementById('titulo').value,
-        director: document.getElementById('director').value,
-        miniatura: document.getElementById('miniatura').value
-    };
-    let mis_peliculas = JSON.parse(localStorage.mis_peliculas);
-    mis_peliculas.push(nuevaPelicula);
-    localStorage.mis_peliculas = JSON.stringify(mis_peliculas);
-    indexContr();
 };
 
 const editContr = async (i) => {
     try {
         const { data: peliculas, error } = await supabase.from('peliculas').select('*');
-
+        
         if (error) {
             throw error;
         }
@@ -168,15 +192,12 @@ const editContr = async (i) => {
     } catch (error) {
         console.error('Error al obtener la película:', error.message);
     }
-const editContr = (i) => {
-    let pelicula = JSON.parse(localStorage.mis_peliculas)[i];
-    document.getElementById('main').innerHTML = editView(i, pelicula);
 };
 
 const updateContr = async (i) => {
     try {
         const { data: peliculas, error } = await supabase.from('peliculas').select('*');
-
+        
         if (error) {
             throw error;
         }
@@ -187,7 +208,7 @@ const updateContr = async (i) => {
         mis_peliculas[i].miniatura = document.getElementById('miniatura').value;
 
         const { data: updatedPeliculas, updateError } = await supabase.from('peliculas').upsert(mis_peliculas);
-
+        
         if (updateError) {
             throw updateError;
         }
@@ -196,13 +217,6 @@ const updateContr = async (i) => {
     } catch (error) {
         console.error('Error al actualizar la película:', error.message);
     }
-const updateContr = (i) => {
-    let mis_peliculas = JSON.parse(localStorage.mis_peliculas);
-    mis_peliculas[i].titulo    = document.getElementById('titulo').value;
-    mis_peliculas[i].director  = document.getElementById('director').value;
-    mis_peliculas[i].miniatura = document.getElementById('miniatura').value;
-    localStorage.mis_peliculas = JSON.stringify(mis_peliculas);
-    indexContr();
 };
 
 const deleteContr = async (i) => {
@@ -210,7 +224,7 @@ const deleteContr = async (i) => {
         let confirmacion = confirm("¿Estás seguro de que quieres eliminar esta película?");
         if (confirmacion) {
             const { data: peliculas, error } = await supabase.from('peliculas').select('*');
-
+            
             if (error) {
                 throw error;
             }
@@ -219,7 +233,7 @@ const deleteContr = async (i) => {
             mis_peliculas.splice(i, 1);
 
             const { data: deletedPeliculas, deleteError } = await supabase.from('peliculas').upsert(mis_peliculas);
-
+            
             if (deleteError) {
                 throw deleteError;
             }
@@ -228,13 +242,6 @@ const deleteContr = async (i) => {
         }
     } catch (error) {
         console.error('Error al eliminar la película:', error.message);
-const deleteContr = (i) => {
-    let confirmacion = confirm("¿Estás seguro de que quieres eliminar esta película?");
-    if (confirmacion) {
-        let mis_peliculas = JSON.parse(localStorage.mis_peliculas);
-        mis_peliculas.splice(i, 1);
-        localStorage.mis_peliculas = JSON.stringify(mis_peliculas);
-        indexContr();
     }
 };
 
@@ -247,15 +254,11 @@ const resetContr = async () => {
         }
     } catch (error) {
         console.error('Error al restablecer las películas originales:', error.message);
-const resetContr = () => {
-    let confirmacion = confirm("¿Estás seguro de que quieres restaurar las películas originales?");
-    if (confirmacion) {
-        localStorage.mis_peliculas = JSON.stringify(mis_peliculas_iniciales);
-        indexContr();
     }
 };
 
-@@ -264,16 +166,15 @@ const matchEvent = (ev, sel) => ev.target.matches(sel);
+// ROUTER de eventos
+const matchEvent = (ev, sel) => ev.target.matches(sel);
 const myId = (ev) => Number(ev.target.dataset.myId);
 
 document.addEventListener('click', ev => {
@@ -270,15 +273,5 @@ document.addEventListener('click', ev => {
 });
 
 // Inicialización
-    if      (matchEvent(ev, '.index'))  indexContr  ();
-    else if (matchEvent(ev, '.edit'))   editContr   (myId(ev));
-    else if (matchEvent(ev, '.update')) updateContr (myId(ev));
-    else if (matchEvent(ev, '.delete')) deleteContr (myId(ev));
-    else if (matchEvent(ev, '.reset'))  resetContr  ();
-    else if (matchEvent(ev, '.show'))   showContr   (myId(ev));
-    else if (matchEvent(ev, '.new'))    newContr    ();
-    else if (matchEvent(ev, '.create')) createContr ();
-})
-
-// Inicialización        
 document.addEventListener('DOMContentLoaded', indexContr);
+
